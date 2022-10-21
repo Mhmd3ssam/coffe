a8b0c87801062f84d0cc3e1e4045ef6aa43a6439
Mhmd <git>
2022-10-21 18:22:05 +0200

    Initial Commit

---
+++ Coffe/CoffeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeApp.swift
+//  Coffe
+//
+//  Created by Mhmd Essam on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coffe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coffe
+//
+//  Created by Mhmd Essam on 21/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
